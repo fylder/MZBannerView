595214728b227b4175c3c6dd21d0166c326a0606
fylder <fylder@163.com>
2017-06-05 16:04:01 +0800

    切换效果

--- mzbanner/src/main/java/com/zhouwei/mzbanner/transformer/CustomTransformer.java
+++ mzbanner/src/main/java/com/zhouwei/mzbanner/transformer/CustomTransformer.java
@@ -9,14 +9,15 @@ import android.view.View;
 
 public class CustomTransformer implements ViewPager.PageTransformer {
     private static final float MIN_SCALE = 0.9F;
+
     @Override
     public void transformPage(View page, float position) {
 
-        if(position < -1){
+        if (position < -1) {
             page.setScaleY(MIN_SCALE);
-        }else if(position<= 1){
-            //
-            float scale = Math.max(MIN_SCALE,1 - Math.abs(position));
+        } else if (position <= 1) {
+
+            float scale = Math.max(MIN_SCALE, 1 - (1 - MIN_SCALE) * Math.abs(position));
             page.setScaleY(scale);
             /*page.setScaleX(scale);
 
@@ -26,7 +27,7 @@ public class CustomTransformer implements ViewPager.PageTransformer {
                 page.setTranslationX(-width * (1 - scale) /2);
             }*/
 
-        }else{
+        } else {
             page.setScaleY(MIN_SCALE);
         }
     }
